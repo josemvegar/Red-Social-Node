9d1fd3b00b735801b5c53189254825197cdd0182
jose <josevega1999,16@gmail.com>
2024-10-17 00:37:10 -0400

    Manejo de imagenes, buscador y proyecto completo.

--- controladores/articulo.js
+++ controladores/articulo.js
@@ -1,6 +1,11 @@
 const {validar} = require("../helpers/validar");
 const Articulo = require("../modelos/Articulo");
 
+// Requerimos FileSystem, la cual es una libreria de node para manejar archivos.
+const fs = require("fs");
+// La libreria path permite tomar un archivo y enviarlo
+const path = require("path");
+
 const prueba = (req, res) =>{
     res.status(200).json({
         mensaje: "Soy una Acción de prueba en mi controlador de Artículos"
@@ -235,6 +240,151 @@ const actualizar = async (req, res) => {
 
 };
 
+const subirArchivo = async (req, res) =>{
+
+    // Configurar multer
+    // Se hizo en el articulo.js de RUTAS (Linea 10)
+
+    // Recoger fichero de imagen subido
+    if (!req.file && !req.files){
+        return res.status(404).json({
+            status: "error",
+            mensaje: "No se han encontrado archivos."
+        });
+    }
+
+    // Conseguir el nombre del archivo o imagen
+    let nombreArchivo = req.file.originalname;
+
+    // Conseguir la extención del archivo
+    let archivoExtencion = nombreArchivo.split("\."); // Con esto tomo lo que está después del punto. Y devuelve arreglo
+    // Arreglo con 2 items, 1 del nombre y el otro de la extención.
+    let extencion = archivoExtencion[1];
+
+    // Comprobar la extención correcta
+    if(extencion!= "png" && extencion!= "jpg" && extencion!= "gif" && extencion!= "jpeg"){
+        // Borrar archivo y dar respuesta
+        fs.unlink(req.file.path, ()=> {
+            return res.status(400).json({
+                status: "error",
+                mensaje: "La extención del archivo no es correcta."
+            });
+        });
+    }else{
+
+        // Actualizar el articulo 
+        try{
+            // Recoger el id a editar
+            let id = req.params.id;
+    
+            // Actualizar datos
+            //const articuloActualizado = await Articulo.findOneAndUpdate({_id: id}, req.body);
+            // Con ese "new: true" mostramos el objeto ya actualizado en la respuesta, ya que si no, muestra el anterior.
+            const articuloActualizado = await Articulo.findOneAndUpdate({_id: id}, {imagen: req.file.filename}, {new: true});
+            // Devolver respuesta
+            if (!articuloActualizado){
+                return res.status(404).json({
+                    status: "error",
+                    mensaje: "No se ha encontrado el articulo " + id + "."
+                });
+            }
+    
+            return res.status(200).json({
+                status: "success",
+                articulo: articuloActualizado,
+                fichero: req.file,
+                mensaje: "Imagen del Articulo " + id + " actualizada."
+            });
+        }
+        catch (error) {
+            return res.status(500).json({
+                status: "error",
+                mensaje: "No se pudo actualizar el articulo."
+            });
+        }
+    }
+};
+
+const imagen = (req, res)=>{
+    let fichero = req.params.fichero.trim(); // trim() para limpiar la cadena.
+    let ruta_fisica = "./imagenes/articulos/" + fichero;
+
+    fs.stat(ruta_fisica, (error, existe)=>{
+        if(existe){
+            // Así se envían archivos
+            return res.sendFile(path.resolve(ruta_fisica))
+        }else{
+            return res.status(404).json({
+                status: "error",
+                mensaje: "La imagen no existe.",
+                ruta: ruta_fisica
+            });
+        }
+    });
+
+}
+
+/*const imagen = (req, res) => {
+    let fichero = req.params.fichero.trim(); // nombre del archivo desde los params
+    let ruta_fisica = path.resolve(__dirname, '../imagenes/articulos/', fichero); 
+
+    // Verificamos si el archivo existe
+    fs.access(ruta_fisica, fs.constants.F_OK, (error) => {
+        if (error) {
+            // Archivo no encontrado
+            return res.status(404).json({
+                status: "error",
+                mensaje: "La imagen no existe.",
+                ruta: ruta_fisica
+            });
+        }
+
+        // Si existe, enviamos el archivo
+        return res.sendFile(ruta_fisica, (err) => {
+            if (err) {
+                console.error("Error al enviar el archivo:", err);
+                res.status(500).json({
+                    status: "error",
+                    mensaje: "Hubo un problema al enviar el archivo."
+                });
+            }
+        });
+    });
+};*/
+
+const buscador = (req, res) =>{
+
+    // Sacar el string a buscar
+    let busqueda = req.params.key;
+
+    // Find a la base de datos con condicion
+    Articulo.find({
+        "$or" : [
+            {titulo: {"$regex" : busqueda, "$options": "i"}},
+            {contenido: {"$regex" : busqueda, "$options": "i"}}
+        ]
+    }).sort({fecha: -1}).exec().then(articulos => {
+        if(!articulos || articulos.length==0){
+            return res.status(404).json({
+                status: "404",
+                mensaje: "No se han encontrado articulos."
+            })
+        }
+
+        return res.status(200).json({
+            status: "success",
+            articulos: articulos
+        })
+
+    }).catch(error => {
+        return res.status(500).json({
+            status: "500",
+            mensaje: "Error al buscar articulos articulos."
+        })
+    });
+
+}
+
 module.exports = {
     prueba,
     curso,
@@ -242,5 +392,8 @@ module.exports = {
     listar,
     unArticulo,
     borrar,
-    actualizar
+    actualizar,
+    subirArchivo,
+    imagen,
+    buscador
 };
--- rutas/articulo.js
+++ rutas/articulo.js
@@ -6,6 +6,20 @@
 const {Router} = require("express");
 const router = Router();
 
+// Importamos el multer para el manejo de imagenes
+const multer = require("multer");
+// Crear el almacenamiento (Configurar donde se guardan las imagenes)
+const almacenamiento = multer.diskStorage({
+    destination: function (req, file, cb) { //cb indica cual es el destino de la subida
+        cb(null, './imagenes/articulos/'); // El primer parametro siempre es null
+    },
+    filename:  function (req, file, cb) {
+        cb(null, "articulo" + Date.now() + file.originalname)
+    }
+});
+// Le indicamos a multer cual es el almacenamiento
+const subidas = multer({storage: almacenamiento})
+
 // Cargar controlador de articulos
 const ArticuloController = require("../controladores/articulo.js");
 
@@ -24,5 +38,15 @@ router.get("/articulos/:ultimos?" , ArticuloController.listar)
 router.get("/articulo/:id" , ArticuloController.unArticulo);
 router.delete("/articulo/:id" , ArticuloController.borrar);
 router.put("/articulo/:id" , ArticuloController.actualizar);
+
+// En esta ruta indicamos un middleware antes de que se ejecute la ruta, diciendo que_
+//subidas es donde está configurado el multer
+// single, porque es un solo archivo
+// "file0" es el nombre del parametro recibido por post
+router.post("/subir-imagen/:id", [subidas.single("file0")] , ArticuloController.subirArchivo);
+router.get("/imagen/:fichero" , ArticuloController.imagen);
+
+// BUSCADOR
+router.get("/buscar/:key" , ArticuloController.buscador);
 
 module.exports = router;
