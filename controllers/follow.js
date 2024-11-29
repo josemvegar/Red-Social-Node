77dec31cd27f58d1ce864acd0ab1dc7919aa7437
jose <josevega1999,16@gmail.com>
2024-11-28 23:43:15 -0400

    Modelo de Follow y Acción de seguir usuario lista.

--- controllers/follow.js
+++ controllers/follow.js
@@ -1,9 +1,77 @@
+// Importar Modelo
+const Follow = require("../models/Follow");
+const User = require("../models/User");
+
 // Acciones de prueba
 const pruebaFollow = (req, res) => {
     return res.status(200).send("<h1>Mensaje de prueba en Follow</h1>");
 };
 
+// Acción de guardar un follow (Seguir)
+const save = async (req,res) => {
+    // Conseguir datos del body
+    let params = req.body;
+
+    // Sacar id del usuario identificado
+    const identity = req.user;
+
+    // Validar tamaño del id
+    if(params.followed.length != 24){
+        return res.status(400).send({
+            status: "error",
+            message: "Id de usuario no válido."
+        });
+    }
+
+    // Crear Objeto con modelo Follow
+    let userToFollow = new Follow({
+        user: identity.id,
+        followed: params.followed
+    });
+
+    // Guardar el objeto en la bd
+    const followedSearch = await User.findById(params.followed)
+    const userFollowed = await Follow.find({$and: [{user: identity.id},{followed: params.followed}]});
+
+    try{
+        if(userFollowed.length > 0){
+            return res.status(400).send({
+                status: "error",
+                message: "Ya sigues a este usuario."
+            });
+        }
+        if(!followedSearch){
+            return res.status(400).send({
+                status: "error",
+                message: "Id de usuario no encontrado."
+            });
+        }
+        const followSaved = await userToFollow.save();
+        if(!followSaved){
+            return res.status(400).send({
+                status: "error",
+                message: "No se ha podido seguir al usuario."
+            });
+        }
+        return res.status(200).send({
+            status: "success",
+            userToFollow
+        });
+    }catch (error){
+        return res.status(400).send({
+            status: "error",
+            message: "No se ha podido seguir al usuario."
+        });
+    }
+}
+// Acción de borrar un follow (Dejar de seguir)
+
+// Listado de usuarios que estoy siguiendo
+
+// Listado de usuarios que me siguen
+
 // Esportar acciones
 module.exports = {
-    pruebaFollow
+    pruebaFollow,
+    save
 };
--- routes/follow.js
+++ routes/follow.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const followController = require("../controllers/follow");
+const auth = require("../middlewares/auth");
 
 // Definir rutas
 router.get("/prueba-follow", followController.pruebaFollow);
+router.post("/save", auth.auth, followController.save);
 
 // Exportar el router
 module.exports = router;
